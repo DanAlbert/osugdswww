ba681e34ed3e8be12867fb09291820b8a11f2227
Dan Albert <dan@gingerhq.net>
2011-06-06 00:54:42 -0700

    The resources page now pulls from the database.

--- resources.php
+++ resources.php
@@ -26,18 +26,82 @@ include 'nav.php';
 	</div>
 	
 	<h2>Presentations</h2>
-	<ul>
-		<li><a href="https://docs.google.com/present/edit?id=0AaQCl-92YIEcZGd0YzJ4OG1fMjE0dzQ1NjVnag&hl=en">24 January 2011 - Graphics and Animation in Games</a></li>
+	<?php
+		require_once 'mysql.php';
+		
+		$con = dbConnect();
+		if (!$con)
+		{
+			die('Could not connect to database server.');
+		}
+		
+		$query = "SELECT Title, Lang, URL, PresentedOn FROM Presentations ORDER BY Lang ASC, PresentedOn DESC;";
+		$result = mysql_query($query, $con);
+		
+		$lastLang == '';
+		while ($row = mysql_fetch_array($result))
+		{
+			if ($row['Lang'] != $lastLang)
+			{
+				if ($lastLang != '')
+				{
+					print '</ul>';
+				}
+				print '<h3>' . $row['Lang'] . ' Presentations</h3><ul>';
+				$lastLang = $row['Lang'];
+			}
+			
+			$dateComp = explode('-', $row['PresentedOn']);
+			$year = $dateComp[0];
+			$month = date("M", mktime(0, 0, 0, $dateComp[1], 1, 2005));
+			$day = $dateComp[2];
+			
+			$date = "$day $month $year";
+			
+			print '<li><a href="' . $row['URL'] . '">' . $date . ' - ' . $row['Title'] . '</a></li>';
+		}
+	?>
+	<!--<ul>
+		<!--<li><a href="https://docs.google.com/present/edit?id=0AaQCl-92YIEcZGd0YzJ4OG1fMjE0dzQ1NjVnag&hl=en">24 January 2011 - Graphics and Animation in Games</a></li>
 		<li><a href="/osugds/resources/Gaming%20Input%20and%20the%20Arcade.pptx">14 February 2011 - Gaming Input and the Arcade</a> - <a href="/osugds/resources/lesson03.zip">Source Code</a></li>
 		<li>28 February 2011 - OpenGL - <a href="/osugds/resources/OpenGLDemo.tar.bz2">Source Code</a> (Note: You will need to place a copy of glut.h in C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Include\gl)</li>
-	</ul>
+	</ul>-->
 	
 	<h2>Tutorials</h2>
-	<h3>Flash Tutorials</h3>
+	<?php
+		require_once 'mysql.php';
+		
+		$con = dbConnect();
+		if (!$con)
+		{
+			die('Could not connect to database server.');
+		}
+		
+		$query = "SELECT Title, Lang, URL FROM Tutorials ORDER BY Lang ASC;";
+		$result = mysql_query($query, $con);
+		
+		$lastLang == '';
+		while ($row = mysql_fetch_array($result))
+		{
+			if ($row['Lang'] != $lastLang)
+			{
+				if ($lastLang != '')
+				{
+					print '</ul>';
+				}
+				print '<h3>' . $row['Lang'] . ' Tutorials</h3><ul>';
+				$lastLang = $row['Lang'];
+			}
+			
+			print '<li><a href="' . $row['URL'] . '">' . $row['Title'] . '</a></li>';
+		}
+	?>
+	<!--<h3>Flash Tutorials</h3>
 	<ul>
 		<li><a href="/osugds/tutorials/flash/tutorial1.php">Tutorial 1</a></li>
 		<li><a href="/osugds/tutorials/flash/tutorial2.php">Tutorial 2</a></li>
 		<li><a href="/osugds/tutorials/flash/tutorial3.php">Tutorial 3</a></li>
+	</ul>-->
 	<div class="clear"></div>
 </div> <!-- main -->
 
